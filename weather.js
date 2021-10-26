e81689104839eda677d697f3479ee3f7c038544b
sahangamlathbandara <70343302+sahangamlathbandara@users.noreply.github.com>
2021-10-26 19:06:23 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -45,8 +45,8 @@ let weather = {
         //console.log(name,icon,description,temp,humidity,speed);
         document.querySelector(".city").innerText = name;
         document.querySelector(".country").innerText = country;
-        document.getElementById("country").title = dat[0][1].name;
-        //console.log("Country name is : "+ dat.name);
+        document.getElementById("country").title = dat.name;
+        console.log("Country name is : "+ dat[0][1].name);
         //console.log(id);
 
 
